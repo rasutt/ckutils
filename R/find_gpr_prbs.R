157328a227d5c46817cd898722432f5102b5438c
Robin Aldridge-Sutton <26667354+rasutt@users.noreply.github.com>
2023-07-30 18:55:59 +1200

    Added functions and vignette code for possible genopair probabilities for other basic kinships.

--- R/find_gpr_prbs.R
+++ R/find_gpr_prbs.R
@@ -18,7 +18,8 @@ find_ale_frqs <- function(ind_gts) {
 
   # Combine with frequencies for 0-coded alleles and return
   ale_frqs = rbind(1 - ale_frqs_1, ale_frqs_1)
-  dimnames(ale_frqs) = list(allele_code = paste0("A", 0:1), locus = colnames(ind_gts))
+  dimnames(ale_frqs) =
+    list(allele_code = paste0("A", 0:1), locus = colnames(ind_gts))
   ale_frqs
 }
 
@@ -49,30 +50,25 @@ find_pss_gt_prbs = function(ale_frqs) {
 #' Find possible first genotype probabilities for genopairs, 3 x 3 x n_loci
 #'
 #' @param pss_gt_prbs Possible genotype probabilities
-#' @param L
+#' @param L Number of loci in each genotype
 #'
 #' @return
 #' @export
 #'
 #' @examples
 find_pss_frst_gt_prbs = function(pss_gt_prbs, L) {
-  n.pss.gts = 3
-
   # 3 x L matrix indexed 3 times to fill the three columns.
   array(
-    data = pss_gt_prbs[, rep(1:L, each = n.pss.gts)],
-    dim = c(n.pss.gts, n.pss.gts, L),
-    dimnames = list(
-      first_genotype = c("GT00", "GT01", "GT11"),
-      second_genotype = c("GT00", "GT01", "GT11"),
-      locus = colnames(pss_gt_prbs)
-    )
+    data = pss_gt_prbs[, rep(1:L, each = n_pss_gts)],
+    dim = c(n_pss_gts, n_pss_gts, L),
+    dimnames = pfgps_dimnames
   )
 }
 
 #' Find possible genopair probabilities for unrelated pairs
 #'
-#' @param pss_gt_prbs Possible genotype probabilities
+#' @param pss_frst_gt_prbs Possible genotype probabilities for the first
+#'   genotype in each genopair
 #'
 #' @return Array (Number of possible genotypes x number of possible genotypes x
 #'   number of loci)
@@ -84,6 +80,85 @@ find_pss_gp_prbs_ups = function(pss_frst_gt_prbs) {
   # the respective genotype probabilities, the second found by permuting the
   # array containing the first.
   pss_frst_gt_prbs * aperm(pss_frst_gt_prbs, c(2, 1, 3))
+}
+
+#' Find possible genopair probabilities for parent-offspring pairs
+#'
+#' @inheritParams find_pss_gt_prbs
+#' @inheritParams find_pss_frst_gt_prbs
+#' @inheritParams find_pss_gp_prbs_ups
+#'
+#' @return
+#' @export
+#'
+#' @examples
+find_pss_gp_prbs_pops = function(ale_frqs, L, pss_frst_gt_prbs) {
+  # Conditional probabilities given that the pair are parent and offspring
+  # (unordered). Products of the first genotype probabilities and the
+  # conditional probabilities of the second genotypes given that the pair are
+  # parent and offspring.
+  pss_frst_gt_prbs *
+
+    # Conditional probabilities of second genotype given that the pair are
+    # parent and offspring (unordered).  0.5 for each allele in first genotype
+    # being inherited, multiplied by the probability of the second genotype in
+    # each case, as in table 3, pg. 269, Bravingtion et al. (2016) Close-Kin
+    # Mark-Recapture. Filled into an L x 3 x 3 array which is permuted to the
+    # standard dimensions.
+    aperm(
+      array(
+        # Note: order data enters array is down columns, not across rows
+        c(
+          ale_frqs[1, ], 0.5 * ale_frqs[1, ], rep(0, L),
+          ale_frqs[2, ], 0.5 * colSums(ale_frqs), ale_frqs[1, ],
+          rep(0, L), 0.5 * ale_frqs[2, ], ale_frqs[2, ]
+        ),
+        c(L, n_pss_gts, n_pss_gts)
+      ),
+      c(2, 3, 1)
+    )
+}
+
+#' Find possible genopair probabilities for self-pairs
+#'
+#' @inheritParams find_pss_gp_prbs_pops
+#'
+#' @return
+#' @export
+#'
+#' @examples
+find_pss_gp_prbs_sps = function(pss_frst_gt_prbs, L) {
+  # Conditional probabilities given that the pair are the same individual
+  # sampled twice. Genotype probabilities when the genotypes are the same, and
+  # zero otherwise.
+  pss_gp_prbs_sps = aperm(
+    array(
+      data = cbind(
+        pss_frst_gt_prbs[1, 1, ], 0, 0,
+        0, pss_frst_gt_prbs[2, 2, ], 0,
+        0, 0, pss_frst_gt_prbs[3, 3, ]
+      ),
+      dim = c(L, n_pss_gts, n_pss_gts)
+    ),
+    c(2, 3, 1)
+  )
+  dimnames(pss_gp_prbs_sps) = pfgps_dimnames
+  pss_gp_prbs_sps
+}
+
+#' Find possible genopair probabilities for half-sibling pairs
+#'
+#' @param pss_gp_prbs_ups
+#' @param pss_gp_prbs_pops
+#'
+#' @return
+#' @export
+#'
+#' @examples
+find_pss_gp_prbs_hsps = function(pss_gp_prbs_ups, pss_gp_prbs_pops) {
+  # Conditional probabilities given that the pair are half-siblings.  Average of
+  # probabilities for unrelated and parent-offspring pairs.
+  (pss_gp_prbs_ups + pss_gp_prbs_pops) / 2
 }
 
 # Function to find possible genotype probabilities for all studies
@@ -116,34 +191,6 @@ FindPssGpPsUPsAry = function(pss.gt.1.prbs.ary) {
 }
 
 # Function to find possible genopair probabilities for parent-offspring pairs
-FindPssGpPsPOPs = function(ale_frqs, L, pss.gt.1.prbs) {
-  # Conditional probabilities given that the pair are parent and offspring
-  # (unordered). Products of the first genotype probabilities and the
-  # conditional probabilities of the second genotypes given that the pair are
-  # parent and offspring.
-  pss.gt.1.prbs *
-
-    # Conditional probabilities of second genotype given that the pair are
-    # parent and offspring (unordered).  0.5 for each allele in first genotype
-    # being inherited, multiplied by the probability of the second genotype in
-    # each case, as in table 3, pg. 269, Bravingtion et al. (2016) Close-Kin
-    # Mark-Recapture. Filled into an L x 3 x 3 array which is permuted to the
-    # standard dimensions.
-    aperm(
-      array(
-        # Note: order data enters array is down columns, not across rows
-        c(
-          ale_frqs[1, ], 0.5 * ale_frqs[1, ], rep(0, L),
-          ale_frqs[2, ], 0.5 * colSums(ale_frqs), ale_frqs[1, ],
-          rep(0, L), 0.5 * ale_frqs[2, ], ale_frqs[2, ]
-        ),
-        c(L, n.pss.gts, n.pss.gts)
-      ),
-      c(2, 3, 1)
-    )
-}
-
-# Function to find possible genopair probabilities for parent-offspring pairs
 FindPssGpPsPOPsAry = function(pss.gt.1.prbs.ary, ale_frqs.ary, L, n.sims) {
   # Conditional probabilities given that the pair are parent and offspring
   # (unordered). Products of the first genotype probabilities and the
@@ -169,24 +216,6 @@ FindPssGpPsPOPsAry = function(pss.gt.1.prbs.ary, ale_frqs.ary, L, n.sims) {
       ),
       c(3, 4, 1, 2)
     )
-}
-
-# Function to find possible genopair probabilities for self-pairs
-FindPssGpPsSPs = function(pss.gt.1.prbs, L) {
-  # Conditional probabilities given that the pair are the same individual
-  # sampled twice. Genotype probabilities when the genotypes are the same, and
-  # zero otherwise.
-  aperm(
-    array(
-      cbind(
-        pss.gt.1.prbs[1, 1, ], 0, 0,
-        0, pss.gt.1.prbs[2, 2, ], 0,
-        0, 0, pss.gt.1.prbs[3, 3, ]
-      ),
-      c(L, n.pss.gts, n.pss.gts)
-    ),
-    c(2, 3, 1)
-  )
 }
 
 # Function to find possible genopair probabilities for self-pairs
